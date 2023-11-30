e5c97dcf5dd730fa8bf267e4a6b36e5eb5065a1d
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-12-01 01:45:04 +0200

    added hpp, xss to backend for security

--- api/index.js
+++ api/index.js
@@ -2,17 +2,33 @@ require('dotenv').config();
 require("express-async-errors");
 const connectDB = require('./db/connect.js');
 const cors = require("cors");
+const path = require('path');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const express = require('express');
 const { errorHandler } = require('./middleware/error.js');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Prevent XSS(Cross Site Scripting) Attacks
+app.use(xss());
+// Protect Http Param Pollution
+app.use(hpp());
+
 // Routes
 app.use('/api/orders', require('./routes/order.route.js'))
 app.use('/api/items', require('./routes/item.route.js'))
 
+const staticPath = path.join(__dirname, '../client/dist');
+app.use(express.static(staticPath));
+
 // Error Handler Middleware
 app.use(errorHandler);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(staticPath, 'index.html'));
+})
 
 const start = async () => {
     try {
